7d4d55b34649c27470a5f3017a8a3f1131d451c3
Your Name <you@example.com>
2019-06-29 16:52:10 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,85 @@
 class Dog 
-    attr_accessor :name, :breed
-    def initialize(name,breed = "Mutt")
+
+
+
+    def initialize(name, breed = "Mutt")
         @name = name
         @breed = breed
     end
 
-    # def name=(name)
-    #     @name = name
-    # end
-    # def name
-    #     @name
-    # end
 
-    # def breed=(breed)
-    #     @breed = breed
-    # end
 
-    # def breed
-    #     @breed
-    # end
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog 
+#     attr_accessor :name, :breed
+#     def initialize(name,breed = "Mutt")
+#         @name = name
+#         @breed = breed
+#     end
+
+#     # def name=(name)
+#     #     @name = name
+#     # end
+#     # def name
+#     #     @name
+#     # end
+
+#     # def breed=(breed)
+#     #     @breed = breed
+#     # end
+
+#     # def breed
+#     #     @breed
+#     # end
 
     
-end
+# end
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,55 @@
 class Person
-    def initialize(name)
-        @name = name
-    end
+
+def initialize(name)
+    @name = name
 end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Person
+#     def initialize(name)
+#         @name = name
+#     end
+# end
